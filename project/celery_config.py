1df964dbd00b0751617fd31039bcfab52a24b2e8
Elbek Allayorov <allayorovelbek@gmail.com>
2024-01-25 16:13:09 +0500

    product parse added to category

--- project/celery_config.py
+++ project/celery_config.py
@@ -19,7 +19,7 @@ app.conf.beat_schedule = {
     # Executes every Monday morning at 7:00 a.m.
     'parse-texnomart-product': {
         'task': 'tasks.parse_technomart_product',
-        'schedule': crontab(minute=0, hour=0),
+        'schedule': crontab(minute=15, hour=16),
     },
     'parse-texnomart-category': {
         'task': 'tasks.parse_technomart_category',
