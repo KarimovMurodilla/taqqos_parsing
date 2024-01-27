15daf239e6dc8eb3e3cd58c5419eb25d2874fafd
Elbek Allayorov <allayorovelbek@gmail.com>
2024-01-27 08:50:10 +0500

    sss

--- project/celery_config.py
+++ project/celery_config.py
@@ -19,7 +19,7 @@ app.conf.beat_schedule = {
     # Executes every Monday morning at 7:00 a.m.
     'parse-texnomart-product': {
         'task': 'tasks.parse_technomart_product',
-        'schedule': crontab(minute=45, hour=3),
+        'schedule': crontab(minute=51, hour=3),
     },
     'parse-texnomart-category': {
         'task': 'tasks.parse_technomart_category',
