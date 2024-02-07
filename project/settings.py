ff8a319dd84644f7ad156be13207d7093d2ff78f
Elbek Allayorov <allayorovelbek@gmail.com>
2024-02-07 11:07:21 +0500

    sss

--- project/settings.py
+++ project/settings.py
@@ -8,5 +8,5 @@ DATABASE_NAME = os.environ.get('DATABASE_NAME', 'taqqos_parsing')
 DATABASE_USER = os.environ.get('DATABASE_USER', 'taqqos_parsing')
 DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD', 'taqqos_parsing')
 DATABASE_HOST = os.environ.get('DATABASE_HOST', 'localhost')
-DATABASE_PORT = os.environ.get('DATABASE_PORT', '5432')
+DATABASE_PORT = os.environ.get('DATABASE_PORT', '5433')
 DATABASE_URL = f'postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
