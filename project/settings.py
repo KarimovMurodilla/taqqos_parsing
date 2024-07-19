b039fc48e4d75f02b2bce7511a97c13d1cffcf6b
KarimovMurodilla <karimovmurodilla15@gmail.com>
2024-07-19 14:42:25 +0500

    some changes

--- project/settings.py
+++ project/settings.py
@@ -9,5 +9,5 @@ DATABASE_USER = os.environ.get('DATABASE_USER', 'taqqos_parsing')
 DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD', 'taqqos_parsing')
 DATABASE_HOST = os.environ.get('DATABASE_HOST', 'localhost')
 DATABASE_PORT = os.environ.get('DATABASE_PORT', '5431')
-# DATABASE_URL = f'postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
-DATABASE_URL = "sqlite:///database.db"
+DATABASE_URL = f'postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
+# DATABASE_URL = "sqlite:///database.db"
